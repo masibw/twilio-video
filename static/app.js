779f50542de765a9f662c21b19e942d83a7e7c1d
masimacbook <masi19bw@gmail.com>
2021-11-21 18:18:09 +0900

    feat: display speak rate when button clicked

--- static/app.js
+++ static/app.js
@@ -24,6 +24,7 @@ const connectButtonHandler = (event) => {
         joinLeaveButton.innerText = "Leave call";
         joinLeaveButton.disabled = false;
         document.getElementById("toggle_audio").disabled = false;
+        document.getElementById("display_speak_rate_button").disabled = false;
       })
       .catch(() => {
         alert("Could not connect to the room. Is the backend running?");
@@ -32,7 +33,6 @@ const connectButtonHandler = (event) => {
       });
   } else {
     disconnect();
-    displaySpeakRate();
     document.getElementById("join_leave").innerText = "Join call";
     connected = false;
   }
@@ -125,6 +125,7 @@ const disconnect = () => {
     .setAttribute("innerHTML", "Mute Audio");
   connected = false;
   document.getElementById("toggle_audio").disabled = true;
+  document.getElementById("display_speak_rate_button").disabled = true;
   updateParticipantCount();
 };
 
@@ -140,18 +141,7 @@ const dominantSpeaker = (participant) => {
   });
 };
 
-const displaySpeakRate = () => {
-  fetch("/speaks")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-const muteAudioHandler = (event) => {
+const toggleAudioHandler = (event) => {
   event.preventDefault();
   room.localParticipant.audioTracks.forEach((publication) => {
     if (publication.track.isEnabled) {
@@ -164,10 +154,57 @@ const muteAudioHandler = (event) => {
   });
 };
 
+const displaySpeakRateHandler = (event) => {
+  event.preventDefault();
+  fetch("/speaks")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.speaks);
+
+      const ctx = document
+        .getElementById("display_speak_rate")
+        .getContext("2d");
+      const myChart = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: Object.keys(data.speaks),
+          datasets: [
+            {
+              label: "発言時間(s)",
+              data: Object.values(data.speaks),
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+            },
+          ],
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 addLocalVideo();
 document
   .getElementById("join_leave")
   .addEventListener("click", connectButtonHandler);
 document
   .getElementById("toggle_audio")
-  .addEventListener("click", muteAudioHandler);
+  .addEventListener("click", toggleAudioHandler);
+document
+  .getElementById("display_speak_rate_button")
+  .addEventListener("click", displaySpeakRateHandler);
